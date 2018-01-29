470a52175337766777f54975ff971321adefbf1e
Piyush Sohal <piyushsohal@Piyushs-MacBook-Pro.local>
2018-01-29 02:41:36 -0800

    added redirection

--- static/js/scrumboard.js
+++ static/js/scrumboard.js
@@ -6,10 +6,11 @@
     .controller("ScrumboardController", [
       "$scope",
       "$http",
+      "$location",
       ScrumboardController
     ]);
 
-  function ScrumboardController($scope, $http) {
+  function ScrumboardController($scope, $http, $location) {
     $scope.add = function (list, title) {
       var card = {
         title: title,
@@ -27,6 +28,13 @@
         }
       );
     };
+
+    $scope.logout = function () {
+      $http.get('/auth_api/logout/')
+        .then(function () {
+            $location.url('/login');
+        });
+    }
 
     $scope.data = [];
 
